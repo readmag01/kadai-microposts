2c7f04efb33028a286dae3907073cfc85b5fc3a6
keisuke <readmag@feitace.xsrv.jp>
2018-09-12 13:41:56 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
+    @if (Auth::check())
+        <?php $user = Auth::user(); ?>
+            {{ $user->name }}
+    @else
 
     <div class="center jumbotron">
         <div class="text-center">
@@ -8,4 +12,7 @@
             {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
+    
+    @endif
+    
 @endsection
