59b09b5150f9b94449875391d24e0ddc8706d8aa
keisuke <readmag@feitace.xsrv.jp>
2018-09-18 12:45:50 +0000

    modify

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,5 +26,6 @@
                 {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
             </div>
         </div>
+        
     @endif
 @endsection
